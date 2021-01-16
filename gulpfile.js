e38eece27e5306d4edbb09f0f68b680b59a2b64d
Illia Soroka <ilycha92@gmail.com>
2021-01-15 22:20:12 -0600

    add gallere slider

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('js', function () {
 	return gulp
 		.src([
 			// 'node_modules/slick-carousel/slick/slick.js',
-			'node_modules/jquery/dist/jquery.min.js',
+			// 'node_modules/jquery/dist/jquery.min.js',
 			//'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
 			'app/js/common.js' // Always at the end
 		])
